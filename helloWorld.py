94f43a7d5e4233f3390d86683976299707cc1ba3
KongXee <kongxee@uri.edu>
2021-03-28 10:58:16 -0700

    add helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,9 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================================================== RESTART: D:/Programs/Python/hellowWorld.py ========================================================
+Hello World
+>>> 
+======================================================== RESTART: D:/Programs/Python/hellowWorld.py ========================================================
+Hello World, my name is Kong and I love Python!
+>>> 
